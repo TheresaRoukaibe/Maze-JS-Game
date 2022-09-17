39157e9430a0e5fdba2b4124730f4c8a91bf0a47
TheresaRoukaibe <theresaroukaibe@gmail.com>
2022-09-17 14:17:26 +0300

    You lost message

--- game.js
+++ game.js
@@ -3,21 +3,20 @@ window.onload = () => {
 const startButton = document.getElementById("start");
 //Still have to make sure of moving with mouse!
 document.addEventListener('mousemove', function(e){
-	var x = e.clientX - 450;
-	var y = e.clientY- 150;
+	const x = e.clientX - 450;
+	const y = e.clientY- 150;
 	startButton.style.left = x + "px";
 	startButton.style.top = y + "px";
 
 let check = e.target.classList.value;
 if(check === "boundary") {
-	//alert("you lost");
-	//var el = document.getElementById("boundary1");
-	//el.style.backgroundColor = "red";
-	
-	let divs = document.getElementsByClassName("boundary");
+	const divs = document.getElementsByClassName("boundary");
 	for(let i =0; i <divs.length; ++i){
 		divs[i].style.backgroundColor= "#FF0000";
 	}
+	const msg = document.getElementById("status");
+	msg.innerText = "You lost :( Stop touching walls !";
+	msg.style.color= "#FF0000";
 	
 }
 });
