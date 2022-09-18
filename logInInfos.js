e2570e281abddbb24c0dc79a483979f003d9c330
TheresaRoukaibe <theresaroukaibe@gmail.com>
2022-09-18 17:57:19 +0300

    fixed issues in email verification

--- logInInfos.js
+++ logInInfos.js
@@ -6,13 +6,43 @@ function signUp(e) {
 	
 	var user = {
 		username: uName,
-		passowrd: uPass,
+		passwrd: uPass,
 		score : points,
 	}; 
 	
-	var json = JSON.stringify(user);
-	localStorage.setItem(uName, json);
-		
+	var test = localStorage.getItem(uName);
+	if(test==null){
+		var json = JSON.stringify(user);
+	localStorage.setItem("user", json);
+	alert("Registration successful");
+    document.location.href="game.html";
+	}else{
+		alert("Account with this username already exists. Sign in with credentials!");
+}
+};
+
+function signIn(e) {
+	event.preventDefault();
+	var uName = document.getElementById("email2").value;
+	var uPass = document.getElementById("pass2").value;
+	var user =JSON.parse(localStorage.getItem('user'));
+	console.log(uName);
+	
+if(user.username == null){
+	alert("No account found. Please Sign Up first!");
+}else{
+	if(uName == user.username){
+	 if(uPass == user.passwrd){
+		 alert("Signed up Successfully !");
+		  document.location.href="game.html";
+	 }else{
+		 alert("Wrong Password. Try again");
+	 }
+}else{
+	alert("Something went wrong. Please make sure of credentials :(");
+}
+}
+	
 };
 
 
